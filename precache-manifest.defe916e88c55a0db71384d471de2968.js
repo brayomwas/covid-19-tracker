8a7eece76c20ef919af70cca7958c51297b6f795
brayomwas <brayomwas95@gmail.com>
2020-06-04 19:54:58 +0300

    Updates

---
+++ precache-manifest.defe916e88c55a0db71384d471de2968.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ca6bcccd5de5359ee769aa859113fa0",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "a9371a15c1ac16477575",
+    "url": "/covid-19-tracker/static/css/main.b93172d5.chunk.css"
+  },
+  {
+    "revision": "9ab0b30e112b05399d68",
+    "url": "/covid-19-tracker/static/js/2.84ede71e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.84ede71e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9371a15c1ac16477575",
+    "url": "/covid-19-tracker/static/js/main.0eeeeb82.chunk.js"
+  },
+  {
+    "revision": "658dc30c1a9c7a60f9af",
+    "url": "/covid-19-tracker/static/js/runtime-main.69449b93.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/corona.d7265326.png"
+  }
+]);
